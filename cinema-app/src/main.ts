54bbf334ca30ca3f697f91e2f845d9519ea1cecd
Bőti Marcell <de9d6m@inf.elte.hu>
2022-06-01 03:44:52 +0200

    Heroku

--- cinema-app/src/main.ts
+++ cinema-app/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:4000'],
+    origin: [process.env['CLIENT'] || 'http://localhost:4000'],
     methods: ['GET', 'POST', 'DELETE', 'PATCH'],
     credentials: true,
   });
